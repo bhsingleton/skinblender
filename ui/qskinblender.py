9ae2ee46ff74b8dbf51cb841c20c746f6fc282c5
Ben Singleton <bhsingleton@gmail.com>
2025-10-24 15:01:03 -0400

    Updating window title.

--- ui/qskinblender.py
+++ ui/qskinblender.py
@@ -120,7 +120,7 @@ class QSkinBlender(qsingletonwindow.QSingletonWindow):
 
         # Initialize main window
         #
-        self.setWindowTitle("|| Skin Blender")
+        self.setWindowTitle("|| Skin-Blender")
         self.setMinimumSize(QtCore.QSize(450, 450))
 
         # Initialize main menu-bar
